52861dfb705966e7095a423e08fa990a9c391929
Jorge Agudo Praena <joragupra@gmail.com>
2020-08-14 19:00:12 +0200

    CircleCI project setup
    Includes scaffolding test class.

---
+++ src/test/java/com/joragupra/AppTest.java
@@ -0,0 +1,13 @@
+package com.joragupra;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+public class AppTest {
+
+    @Test
+    public void addition() {
+        assertEquals(2, 2, "This is a dumb test");
+    }
+
+}
